405d3aa0c7a5360a9caf4262644cc1b2ca7046bb
DyadyaRodya <rodion12344321@mail.ru>
2019-10-05 01:39:00 +0300

    my decision for correcting sentense without checking if it's not correct, but checks if if's empty(then return the same string)

--- correctSentence.js
+++ correctSentence.js
@@ -18,6 +18,7 @@ correctSentence("Greetings, friends.") == "Greetings, friends."
  */
 
 export default function correctSentence(text) {
-  // your solution goes here
+  if(text)  // check if it isn't empty
+    text = text[0].toUpperCase() + text.slice(1) + (text.slice(-1)=='.'?'':'.');
   return text;
 }
