16b18d9ee9b92aeef5419f02d346d106bcfd6505
DyadyaRodya <rodion12344321@mail.ru>
2019-10-22 18:40:37 +0300

    full dialog page

--- web-components/src/components/FormInput.js
+++ web-components/src/components/FormInput.js
@@ -2,17 +2,54 @@ const template = document.createElement('template');
 template.innerHTML = `
     <style>
         input {
-            border: 0;
-            outline: none;
-            width: calc(100% - 2px);
+            color: #656668;
+            outline: 0;
+            flex: auto;
+            height: 100%;
+            font-size: 25px;
         }
 
         :host {
-            display: inline-block;
-            border: 1px solid rgba(25, 25, 25, 0.32);
+            display: flex;
+            border-width:1px;
+            border-style: solid;
+            border-image: linear-gradient(135deg, #e0dcdc, #e3e1e1) 1 2;
+            background-color: #FFFFFF;
+            height: 60px;
+            flex-direction: row;
         }
+        .clipButton{
+            background: url(https://image.flaticon.com/icons/png/512/23/23188.png) no-repeat  center;
+            background-size: 115%;
+        }
+
+        .sendButton{
+            margin: 0 10px;
+            background: url(https://static.thenounproject.com/png/373675-200.png) no-repeat  center;
+            background-color:#FFFFFF;
+            background-size: 100%;
+        }
+
+        .inputButton{
+            height: 100%;
+            width: 50px;
+            cursor: pointer;
+            opacity: 0.85;
+            transition-duration: 0.15s;
+        }
+
+        .inputButton:hover{
+            opacity: 1.0;
+        }
+
+        .inputButton:active{
+            opacity: 0.6;
+        }
+
     </style>
-    <input type="text">
+    <input/>
+        <div class="inputButton clipButton"></div>
+        <div class="inputButton sendButton"></div>
 `;
 
 class FormInput extends HTMLElement {
@@ -22,6 +59,23 @@ class FormInput extends HTMLElement {
         this._shadowRoot.appendChild(template.content.cloneNode(true));
 
         this.$input = this.shadowRoot.querySelector('input');
+        this.$sendButton = this.shadowRoot.querySelector('.sendButton');
+        this.$clipButton = this.shadowRoot.querySelector('.clipButton');
+        this.$sendButton.addEventListener('click', this.onSubmit.bind(this));
+        this.$clipButton.addEventListener('click', this.onclipButton.bind(this));
+        this.$input.addEventListener('keypress', this.onKeyPress.bind(this));
+    }
+
+    onSubmit() {
+        this.dispatchEvent(new Event('onSubmit'));
+    }
+
+    onclipButton() {
+        this.dispatchEvent(new Event('clickclipButton'));
+    }
+
+    onKeyPress(event) {
+        if (event.keyCode === 13) this.onSubmit();
     }
 
     static get observedAttributes() {
@@ -29,6 +83,7 @@ class FormInput extends HTMLElement {
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'value') this.$input.value = newValue;
         this.$input.setAttribute(name, newValue);
     }
 
