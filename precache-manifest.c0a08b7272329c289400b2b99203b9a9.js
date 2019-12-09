44382c77bfe2cbae190f44a1db3078b2112f3749
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-09 12:56:13 +0000

    Deploy DyadyaRodya/2019-2-Atom-Frontend-R-Sharafiev to github.com/DyadyaRodya/2019-2-Atom-Frontend-R-Sharafiev.git:gh-pages

---
+++ precache-manifest.c0a08b7272329c289400b2b99203b9a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07befcd59ae433b8a35099ef393a07ee",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/index.html"
+  },
+  {
+    "revision": "cc3c9ee41ce7e8803936",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/css/main.7da5145f.chunk.css"
+  },
+  {
+    "revision": "bba4f62385f3ab71032a",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/js/2.d090a8f1.chunk.js"
+  },
+  {
+    "revision": "cc3c9ee41ce7e8803936",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/js/main.9d0191bb.chunk.js"
+  },
+  {
+    "revision": "7612805b878ec9b7fab3",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/js/runtime-main.64a80e02.js"
+  },
+  {
+    "revision": "60f2012dc1b054182658c2451910d9cf",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/media/attach_button.60f2012d.svg"
+  },
+  {
+    "revision": "d52bd5b7af9cdf3ed0f3d197ee61d564",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/media/audiorecordstart_button.d52bd5b7.svg"
+  },
+  {
+    "revision": "1a57927e69565e54ccf527b664d74a86",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/media/audiorecordstop_button.1a57927e.svg"
+  },
+  {
+    "revision": "190de80613377259fa8d85edcbb6f0d2",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/media/geolocation_button.190de806.svg"
+  },
+  {
+    "revision": "ba452dc78ae23af7aea02fef3cf716da",
+    "url": "//dyadyarodya.github.io/2019-2-Atom-Frontend-R-Sharafiev/static/media/submit_button.ba452dc7.svg"
+  }
+]);
